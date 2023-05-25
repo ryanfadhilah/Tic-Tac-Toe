12a64ff0b2320726f5d0a4559927fbb76bea9137
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-25 14:21:48 +0700

    improve feature

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+// Sellector
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 
+// memory
 const x_class = 'x'
 const o_class = 'o'
-
 let circleTurn
+const WIN = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // cross
+    [1, 4, 8],
+    [6, 4, 2],
+]
 
+
+// function
 startGame()
 
 function startGame() {
+    console.log('game start !')
+
+    // 
+
     circleTurn = false
     cellElements.forEach((cell) => {
         // once true: only applied once
@@ -28,6 +50,10 @@ function handleClick(e) {
     // apply hover
     setBoardHoverClass()
     // check win
+    if (checkWin(currentClass)) {
+        console.log('Got Winner')
+        endGame(false)
+    }
     // check draw
 }
 
@@ -49,4 +75,27 @@ function setBoardHoverClass() {
     if (!circleTurn) {
         board.classList.add(x_class)
     }
+}
+function checkWin(currentClass) {
+    // some : check all element, atleast one pass, pas = true
+    return WIN.some((combination) => {
+        //  every : test if all elements pass the condition, pass = true
+        return combination.every((i) => {
+            console.log(cellElements[i].classList.contains(currentClass))
+            return cellElements[i].classList.contains(currentClass)
+        })
+    })
+}
+function endGame(draw) {
+    if (draw) {
+        console.log(`no winner`)
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "X`s" : "O`s"} Wins !`
+    }
+    winningMessageElement.classList.add('show')
+
+}
+
+function restart() {
+    location.reload()
 }
